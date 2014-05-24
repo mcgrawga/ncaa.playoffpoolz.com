27b3cd02f750b3029605cb7d50f221b7c8e32543
Garth McGraw <garthmcgraw@statgolf.com>
2014-05-24 13:54:31 -0500

    initial commit

---
+++ app/controllers/staticpages_controller.rb
@@ -0,0 +1,22 @@
+class StaticpagesController < ApplicationController
+  def index
+  end
+  def pricing
+  end
+  def scoring
+  end
+  def howitworks
+  end
+  def notauthorized
+  end
+  def signupthanks
+  end
+  def notpaid
+  end
+  def overmaxplayers
+  end
+  def nomoresignups
+  end
+  def contact
+  end
+end
